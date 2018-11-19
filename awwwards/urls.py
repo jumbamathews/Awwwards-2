a4b10321854547a39595f6fae829479bdf28db27
MaryannGitonga <naygitonga4@gmail.com>
2018-11-19 07:26:00 +0300

    Add registration urls

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -21,5 +21,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls,name='admin'),
     url(r'',include('projects.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout,{'next_page':'/'}),
     
 ]
