c024f3ca34c2cff59193ec81239af0599ef9c874
MaryannGitonga <naygitonga4@gmail.com>
2018-11-19 08:59:45 +0300

    Add new_project url

--- projects/urls.py
+++ projects/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns=[
     url(r'^$',views.index, name='index'),
     url(r'^profile/',views.profile, name='profile'),
+    url(r'^new_project/',views.submit_project, name='submit_project'),
 
 ]
 if settings.DEBUG:
