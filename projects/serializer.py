0f5ae57cf23a7b2fa017867df9e3337457e612d2
MaryannGitonga <naygitonga4@gmail.com>
2018-11-19 07:57:13 +0300

    Add ProjectSerializer serializer

--- projects/serializer.py
+++ projects/serializer.py
@@ -6,3 +6,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ('user','bio','profile_pic','projects')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('project_title','project_description','landing_page','live_site','design','usability','content')
